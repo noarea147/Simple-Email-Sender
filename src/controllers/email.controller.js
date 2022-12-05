158ab3fd9813ac16ff0572b605599d3911914b58
noarea147 <95benmbarek@gmail.com>
2022-12-06 00:44:05 +0100

    fix

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,40 +1,32 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-const replaceString = (EmailBody, name) => {
-  let newEmailBody = EmailBody.replace("[[name]]", name);
-  return newEmailBody;
-};
-
 exports.sendEmail = async (req, res) => {
-  let sendingLogs = [];
   const data = req.body.emails;
   const configs = req.body.smtp;
   const emailData = req.body.emailData;
 
   try {
     let transporter = nodemailer.createTransport({
-      host: `${configs.host}`,
+      host: "vps87356.inmotionhosting.com",
       port: 25,
       secure: false, // true for 465, false for other ports
       auth: {
-        user: `${configs.user}`, // generated ethereal user
-        pass: `${configs.pass}` // generated ethereal password
+        user: "smtp@westchasemarket.com", // generated ethereal user
+        pass: "oj?z_hpJLvny" // generated ethereal password
       }
     });
 
-    for (i = 0; i < data.length; i++) {
       await transporter.sendMail({
         from: `${emailData.senderName} <${configs.user}>`, // sender address
-        to: `${data[i].email}`, // list of receivers
+        to: `${data.email}`, // list of receivers
         subject: `${emailData.subject}`, // Subject line
-        html: `${replaceString(emailData.emailBody, data[i].name)}` // html body
+        html: `${emailData.emailBody}` // html body
       });
-      sendingLogs.push("email to " + data[i].email + " is sent");
-    }
+    
     res.status(200).json({
       message: "emails sent",
-      data: sendingLogs,
+      data: data.email,
       status: "success"
     });
   } catch (err) {
