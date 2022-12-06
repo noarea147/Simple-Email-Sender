f935104d12601fb5676cf8898ba88c2352205150
noarea147 <95benmbarek@gmail.com>
2022-12-06 01:05:10 +0100

    fixes

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ require("dotenv").config();
 // const config = require('./config/database');
 const LOG = require("./logger");
 
-const port = process.env.PORT || 4000;
+const port = 4000;
 app.use(express.json());
 app.use(cors());
 app.listen(port, () => {
-  LOG.info(`Mailing Server started on port ${port} [${process.env.NODE_ENV}]`);
+  LOG.info(`Mailing Server started on port ${port} [production]`);
 });
 
 app.use("/", require("./src/routes/email.routes"));
--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -2,11 +2,20 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 exports.sendEmail = async (req, res) => {
-  const data = req.body.emails;
+  const email = req.body.email;
   const configs = req.body.smtp;
   const emailData = req.body.emailData;
 
   try {
+    // let transporter = nodemailer.createTransport({
+    //   host: "oxa.email",
+    //   port: 25,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: "contact@avoconsulte.com", // generated ethereal user
+    //     pass: "tAqTZ4$zni" // generated ethereal password
+    //   }
+    // });
     let transporter = nodemailer.createTransport({
       host: "vps87356.inmotionhosting.com",
       port: 25,
@@ -17,16 +26,16 @@ exports.sendEmail = async (req, res) => {
       }
     });
 
-      await transporter.sendMail({
-        from: `${emailData.senderName} <${configs.user}>`, // sender address
-        to: `${data.email}`, // list of receivers
-        subject: `${emailData.subject}`, // Subject line
-        html: `${emailData.emailBody}` // html body
-      });
-    
+    let response = await transporter.sendMail({
+      from: `${emailData.senderName} <smtp@westchasemarket.com>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `${emailData.subject}`, // Subject line
+      html: `${emailData.emailBody}` // html body
+    });
+
     res.status(200).json({
       message: "emails sent",
-      data: data.email,
+      data: [email],
       status: "success"
     });
   } catch (err) {
@@ -37,5 +46,5 @@ exports.sendEmail = async (req, res) => {
   }
 };
 exports.home = async (req, res) => {
-  res.send("you are in the right place")
+  res.send("you are in the right place");
 };
